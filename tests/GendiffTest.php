2578c3bb2337e642ab5b25521674d9bd9b88557f
Mike Kolotovich <k2punk@gmail.com>
2025-02-06 16:00:45 +0300

    fix tests

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -4,7 +4,7 @@ namespace Hexlet\Phpunit\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use function Gendiff\Gendiff\generateDiff;
+use function Differ\Differ\genDiff;
 use function Gendiff\ReadFile\getFixturePath;
 
 class GendiffTest extends TestCase
@@ -19,26 +19,26 @@ class GendiffTest extends TestCase
     {
         $file1 = getFixturePath('file1.json');
         $file2 = getFixturePath('file2.json');
-        $this->assertEquals($this->coll, generateDiff($file1, $file2));
+        $this->assertEquals($this->coll, genDiff($file1, $file2));
     }
     public function testGendiffYaml(): void
     {
         $file1 = getFixturePath('file1.yaml');
         $file2 = getFixturePath('file2.yml');
-        $this->assertEquals($this->coll, generateDiff($file1, $file2));
+        $this->assertEquals($this->coll, genDiff($file1, $file2));
     }
     public function testGendiffPlain(): void
     {
         $file1 = getFixturePath('file1.json');
         $file2 = getFixturePath('file2.json');
         $this->coll = file_get_contents(getFixturePath('expected_plain_file.txt'));
-        $this->assertEquals($this->coll, generateDiff($file1, $file2, 'plain'));
+        $this->assertEquals($this->coll, genDiff($file1, $file2, 'plain'));
     }
     public function testGendiffJsonFormatter(): void
     {
         $file1 = getFixturePath('file1.json');
         $file2 = getFixturePath('file2.json');
         $this->coll = file_get_contents(getFixturePath('expected_json_file.json'));
-        $this->assertEquals($this->coll, generateDiff($file1, $file2, 'json'));
+        $this->assertEquals($this->coll, genDiff($file1, $file2, 'json'));
     }
 }
